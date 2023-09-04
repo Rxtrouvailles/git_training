23732391c960b6d5adf5d20404d44bc1f0ce8847
rxtrouville <ofjuancarlos1@gmail.com>
2023-09-04 10:59:11 -0400

    ahi voy

--- codigoDePrueba.py
+++ codigoDePrueba.py
@@ -1,6 +1,52 @@
-sfsd
-sdkasjdka
-codiguito
-sad
-235
-print("hola")
+# input=open('/home/JC/Escritorio/entrada','r')
+# output=open('/home/JC/Escritorio/salida','w')
+
+# inputline=input.readline()
+
+# # print(type(inputline))
+
+# print(inputline.split())
+# print('--------*--------')
+
+
+# while (inputline):
+#   print(inputline)
+#   inputline=input.readline()
+  
+
+# print('-----------------')
+# print(inputline.split())
+
+
+# crea una matriz de n elementos
+
+
+
+# m=int(input('valos de filas?: '))
+# n=int(input("valor de columnas?: "))
+# arreglo=[[0] * n for i in range (0,m)]
+
+# for i in range(0,n)
+
+
+# print(arreglo[2][3])
+
+
+# import tkinter
+
+# root=tkinter.Tk().mainloop()
+# # root.mainloop()
+
+import tkinter as tk
+
+from tkinter import ttk
+
+root = tk.Tk()
+frm = ttk.Frame(root, padding=10)
+frm.grid()
+
+ttk.Label(frm, text="Hola MG! \n Dame un abrazito!\n si me lo das maybe dios te perdone tus pecados").grid(column=0, row=0)
+ttk.Button(frm, text="si", command=root.destroy).grid(column=1, row=0)
+ttk.Button(frm, text="no", command=root.destroy).grid(column=1, row=1)
+
+root.mainloop()
